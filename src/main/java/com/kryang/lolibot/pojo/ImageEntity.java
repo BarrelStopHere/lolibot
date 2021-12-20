fbda105aa3d2a2d96774f6dfb190819c707544c7
ykr <1045607920@qq.com>
2021-12-20 19:09:16 +0800

    pojo

--- src/main/java/com/kryang/lolibot/pojo/ImageEntity.java
+++ src/main/java/com/kryang/lolibot/pojo/ImageEntity.java
@@ -1,7 +1,16 @@
 package com.kryang.lolibot.pojo;
 
-import java.io.Serializable;
+import lombok.Data;
 
+import java.io.Serializable;
+import java.util.Date;
+
+@Data
 public class ImageEntity implements Serializable {
     private final static long serialVersionUID = -4654565537628701302L;
+    private int id;
+    private String tag;
+    private String image;
+    private String group;
+    private Date create;
 }
---
+++ src/main/java/com/kryang/lolibot/pojo/UserEntity.java
@@ -0,0 +1,10 @@
+package com.kryang.lolibot.pojo;
+
+import lombok.Data;
+
+import java.io.Serializable;
+
+@Data
+public class UserEntity implements Serializable {
+    private final static long serialVersionUID = -4654565537628701302L;
+}
